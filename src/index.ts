e005ca1969b71bb614aa0e53bc78dce2ff7b923d
sangoandco <msangobiyi@gmail.com>
2023-04-16 22:57:33 +0100

    FEAT: creation & configuration of swagger.ts FIX: update of index file with needed routes & swagger specs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import bookRoutes from './controllers/bookRoutes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from '../swagger';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req:any, res:any) => {
-    res.send('Hello World!');
-});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(bookRoutes);
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
